158e78a9be3c2611027147b79d1e831a9238a4f0
Jarred Capellman <jcapellman@hotmail.com>
2023-05-11 06:37:19 -0500

    Added: placeholder for autogen

---
+++ pyGen/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace pyGen
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // TODO: Load C# class into an object
+
+            // TODO: Parse for functions with the appropriate function decorator
+
+            // TODO: Create a Python template file to mail merge
+
+            // TODO: Populate the fields with the output from the function
+        }
+    }
+}
